f1c90521fb0181252fe29e320354857f1e716d21
costaganteng <ardianus.caesar3@smk.belajar.id>
2024-10-22 08:40:27 +0700

    logo

--- admin/index.php
+++ admin/index.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Login</title>
-    <!-- Bootstrap CSS -->
+    <link rel="shortcut icon" href="../assets/img/logo.png" type="image/x-icon">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
